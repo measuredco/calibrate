9afc23cdd8ac78396301b5eb06503ac81bf8a8d7
monospaced <scott@monospaced.com>
2026-03-09 19:23:56 +0000

    feat: add Storybook a11y addon and expand CI quality gates

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
-  addons: ["@storybook/addon-docs"],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
   framework: "@storybook/web-components-vite",
   stories: ["../src/**/*.stories.ts"],
 };
